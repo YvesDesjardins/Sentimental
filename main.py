e59802b87ac6d7df13d33e439b5b872ea51144f6
Yves Desjardins <45479020+YvesDesjardins@users.noreply.github.com>
2020-09-29 13:18:12 -0700

    changes to enable relative imports

---
+++ main.py
@@ -0,0 +1,19 @@
+from services.reddit import get_reddit_client
+from utils.posts import process_post
+from utils.comments import get_comments
+from services.db import close_db_connection
+
+def run():
+  reddit = get_reddit_client()
+  subreddit = reddit.subreddit("wallstreetbets")
+  posts = subreddit.hot(limit=10)
+
+  print("Updating DB and preping model, this may take a minute or two")
+  for post in posts:
+      process_post(post)
+      get_comments(post)
+
+  close_db_connection()
+
+if __name__ == "__main__":
+    run()
