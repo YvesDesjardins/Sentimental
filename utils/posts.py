b4e0d3996b02fbfdbf6dacebe046f231b4022cd0
Yves Desjardins <45479020+YvesDesjardins@users.noreply.github.com>
2020-09-29 14:01:25 -0700

    implement inserts to db for posts

--- utils/posts.py
+++ utils/posts.py
@@ -1,12 +1,18 @@
-from services.db import get_db_client
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from services.db import select_where_id, insert
 
-db = get_db_client()
+table = "Posts"
 
 def process_post(post):
-    print("Post -----------------")
-    print(post.id)        # Output: the post's id
-    print(post.title)     # Output: the post's title
-    print(post.score)     # Output: the post's score
-    print(post.selftext)  # Output: the post's selftext
-    print(post.url)       # Output: the URL the post points to
-                          # or the post's URL if it's a self post
+    records = select_where_id(table, post.id)
+
+    # insert if no records exist
+    if len(records) == 0:
+        print("insert")
+        options = "(id, author_id, title, score, selftext, url) VALUES (?, ?, ?, ?, ?, ?)"
+        data_tuple = (post.id, post.author.id, post.title, post.score, post.selftext, post.url)
+        insert(table, options, data_tuple)
+    else:
+        print("update")
+        # todo - might want to update existing?
