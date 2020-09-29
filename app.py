3ba87029531c745a16a7536beee6a67f89333184
Yves Desjardins <45479020+YvesDesjardins@users.noreply.github.com>
2020-09-29 12:58:02 -0700

    close db connection after processing

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from services.reddit import get_reddit_client
 from utils.posts import process_post
 from utils.comments import get_comments
+from services.db import close_db_connection
 
 reddit = get_reddit_client()
 subreddit = reddit.subreddit("wallstreetbets")
@@ -10,3 +11,5 @@ print("Updating DB and preping model, this may take a minute or two")
 for post in posts:
     process_post(post)
     get_comments(post)
+
+close_db_connection()
