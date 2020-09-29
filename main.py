4124a1814d8f113206964b430a2a3dfcf3c66504
Yves Desjardins <45479020+YvesDesjardins@users.noreply.github.com>
2020-09-29 14:47:46 -0700

    increase limit

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from services.db import close_db_connection
 def run():
   reddit = get_reddit_client()
   subreddit = reddit.subreddit("wallstreetbets")
-  posts = subreddit.hot(limit=25)
+  # make sure to increase praw's rate limit in env/lib/praw/praw.ini, 45 has worked for me
+  posts = subreddit.hot(limit=100)
 
   print("Updating DB and preping model, this may take a minute or two")
   for post in posts:
