dbcbea1d9cf58f1781aabba26dc2c676c88f278a
Yves Desjardins <45479020+YvesDesjardins@users.noreply.github.com>
2020-09-30 16:12:13 -0700

    move limit back to 100

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def run():
     reddit = get_reddit_client()
     subreddit = reddit.subreddit("wallstreetbets")
     # make sure to increase praw's rate limit in env/lib/praw/praw.ini, 45 has worked for me along with ratelimit 10
-    posts = subreddit.search("flair:DD", sort="hot", time_filter="month", limit=2)
+    posts = subreddit.search("flair:DD", sort="hot", time_filter="month", limit=100)
 
     print("Updating DB and preping model, this may take a couple minutes \n")
     for post in posts:
